ce1466848a4fb26df24c638c3988584d5b8e96f5
Clayman <Clayman@DESKTOP-DM8E77U>
2024-02-01 18:49:49 +0300

    FOR and PSU

---
+++ ControlStructures/FOR/main.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define WHILE
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef WHILE
+	int i = 0;	//счетчик цикла
+	int n;		//количество итераций
+	cout << "Введите количетво итераций: "; cin >> n;
+	while (i < n)
+	{
+		cout << "Hello\n";
+		i++;
+	}
+#endif // WHILE
+
+	int n;
+	cout << "Введите количество итераций: "; cin >> n;
+	for (
+		int i = 0; 
+		i < n; 
+		i++
+		)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
+}
