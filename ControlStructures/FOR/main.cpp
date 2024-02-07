b7891eee4853c1f8ca7585a109e8434b14e2233f
Clayman <Clayman@DESKTOP-DM8E77U>
2024-02-07 14:42:58 +0300

    FOR practice.

--- ControlStructures/FOR/main.cpp
+++ ControlStructures/FOR/main.cpp
@@ -5,6 +5,13 @@ using std::cout;
 using std::endl;
 
 //#define WHILE
+//#define FOR_SYNTAX
+
+//#define FACTORIAL
+//#define POWER
+//#define ASCII
+#define FIBONACCI_1
+#define FIBONACCI_2
 
 void main()
 {
@@ -21,15 +28,71 @@ void main()
 	}
 #endif // WHILE
 
+#ifdef FOR_SYNTAX
 	int n;
 	cout << "Введите количество итераций: "; cin >> n;
 	for (
-		int i = 0; 
-		i < n; 
+		int i = 0;
+		i < n;
 		i++
 		)
 	{
 		cout << i << "\t";
 	}
 	cout << endl;
+#endif // FOR_SYNTAX
+
+#ifdef FACTORIAL
+	char a = 100;
+	a += 200;
+	cout << (int)a << endl;
+	int n;
+	cout << "Введите число: "; cin >> n;
+	double f = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i << "! = ";
+		f *= i;
+		cout << f << "\n";
+	}
+	cout << endl;
+#endif // FACTORIAL
+
+#ifdef POWER
+	double a;		//основание степени (base)
+	int n;		//показатель степени(exponent)
+	double N = 1;	//степень	(power)
+	cout << "Введите основание степени: "; cin >> a;	//Enter power base
+	cout << "Введите показатель степени:";cin >> n;	//Enter power exponent
+	if (n < 0)
+	{
+		a = 1 / a;
+		n = -n;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		N *= a;
+	}
+	cout << N << endl;
+#endif // POWER
+
+#ifdef ASCII
+	for (int i = 0; i < 256; i++)
+	{
+		if (i % 16 == 0)cout << endl;
+		cout << (char)i << " ";
+	}
+#endif // ASCII
+
+#ifdef FIBONACCI_1
+	int n;
+	cout << "Введите предельное число: "; cin >> n;
+	//Comma operator
+	for (int a = 0, b = 1, c = a + b; a <= n; a = b, b = c, c = a + b)
+	{
+		cout << a << "\t";
+	}
+	cout << endl;
+#endif // FIBONACCI_1
+
 }
