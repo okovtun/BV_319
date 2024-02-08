20701d1edc329ced619802b134dd7e3accc3eec8
Clayman <Clayman@DESKTOP-DM8E77U>
2024-02-08 21:43:57 +0300

    Multiplication table DONE

--- ControlStructures/FOR/main.cpp
+++ ControlStructures/FOR/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include<iostream>
+#include<ctime>
 using namespace std;
 using std::cin;
 using std::cout;
@@ -10,8 +11,10 @@ using std::endl;
 //#define FACTORIAL
 //#define POWER
 //#define ASCII
-#define FIBONACCI_1
+//#define FIBONACCI_1
 #define FIBONACCI_2
+//#define SIMPLEX_NUMBERS
+#define MULTIPLICATION_TABLE
 
 void main()
 {
@@ -63,7 +66,7 @@ void main()
 	int n;		//показатель степени(exponent)
 	double N = 1;	//степень	(power)
 	cout << "Введите основание степени: "; cin >> a;	//Enter power base
-	cout << "Введите показатель степени:";cin >> n;	//Enter power exponent
+	cout << "Введите показатель степени:"; cin >> n;	//Enter power exponent
 	if (n < 0)
 	{
 		a = 1 / a;
@@ -94,5 +97,52 @@ void main()
 	}
 	cout << endl;
 #endif // FIBONACCI_1
+
+#ifdef SIMPLEX_NUMBERS
+	int n;
+	cout << "Введите предельное число: "; cin >> n;
+	system("COLOR 0A");
+	int simplex_counter = 0;
+	time_t start = clock();
+	for (int i = 0; i <= n; i++)
+	{
+		bool simple = true;	//Предположим, что число простое,
+		//но это нужно проветрить:
+		for (int j = 2; j < sqrt(i); j++)
+			//Функция sqrt() возвращает квадратный корень числа
+		{
+			if (i % j == 0)
+			{
+				simple = false;
+				break;	//Ключевое слово 'break', прерывает текущую итерацию и все последующие
+			}
+		}
+		if (simple)simplex_counter++;
+		//cout << (simplex_counter++, i) << "\t";
+	}
+	time_t end = clock();
+	cout << endl;
+	cout << "Amount of simplex numbers: " << simplex_counter << endl;
+	cout << "Calculated by " << double(end - start) / CLOCKS_PER_SEC << " seconds" << endl;
+#endif // SIMPLEX_NUMBERS
+
+#ifdef MULTIPLICATION_TABLE
+	for (int i = 1; i <= 10; i++)	//Outer for
+	{
+		cout << "Таблица умножения на " << i << ":\n";
+		for (int j = 1; j <= 10; j++)//Inner for
+		{
+			if (i < 10)cout << " ";
+			cout << i << " * ";
+			if (j < 10)cout << " ";
+			cout << j << " = ";
+			if (i*j < 100)cout << " ";
+			if (i*j < 10)cout << " ";
+			cout << i * j << endl;
+		}
+		cout << endl;
+	}
+#endif // MULTIPLICATION_TABLE
+
 
 }
