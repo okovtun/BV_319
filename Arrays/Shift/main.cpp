f1e2bdb78efee2720fe3da8bf0361e31243a1e4a
Clayman <Clayman@DESKTOP-DM8E77U>
2024-03-19 20:32:01 +0300

    ShiftLeft DONE

--- Arrays/Shift/main.cpp
+++ Arrays/Shift/main.cpp
@@ -11,7 +11,7 @@ void main()
 	//int() - значение по умолчанию для типа 'int'
 	//double() - значение по умолчанию для типа 'double'
 	const int n = 10;
-	int arr[n] = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+	int arr[n] = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
 
 	//Вывод исходного массива на экран:
 	for (int i = 0; i < n; i++)
@@ -21,6 +21,17 @@ void main()
 	cout << endl;
 
 	//Сдвиг массива:
+	int number_of_shifts;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+	}
 
 	//Вывод сдвинутого массива на экран:
 	for (int i = 0; i < n; i++)
