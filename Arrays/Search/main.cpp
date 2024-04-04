35a8eff2fb0da57ecab95166c5c78992e3802c2a
Clayman <Clayman@DESKTOP-DM8E77U>
2024-04-04 19:44:15 +0300

    Search DONE

---
+++ Arrays/Search/main.cpp
@@ -0,0 +1,47 @@
+﻿//Search
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 10;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 5;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	//		Поиск повторений:
+	for (int i = 0; i < n; i++)
+	{
+		//arr[i] - выбранный элемент
+		bool met_before = false;	//предполагаем, что выбранное значение ранее не встречалось,
+		//но это нужно проветрить:
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				met_before = true;
+				break;				//break - прерывает текущую итерацию и все последующие.
+			}
+		}
+		if (met_before)continue;	//continue - прерывает текущую итерацию, и переходит к следующей.
+
+		int counter = 0;	//счетчик повторений
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[i] == arr[j])counter++;
+		}
+		//if (counter > 1) printf("Значение %i встречается %i раз\n", arr[i], counter);
+		if (counter) printf("Значение %i повторяется %i раз\n", arr[i], counter);
+		//https://legacy.cplusplus.com/reference/cstdio/printf/
+//		if (counter)cout << "Значение " << arr[i] << " повторяется " << counter << " раз" << endl;
+//		if (counter)cout << "Value " << arr[i] << " repeats " << counter << " times" << endl;
+	}
+}
