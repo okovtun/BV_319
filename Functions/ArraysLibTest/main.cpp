1c3b03cbc45f83fb7b92d01c9382ab8f05c3c5a1
Clayman <Clayman@DESKTOP-DM8E77U>
2024-04-27 21:32:50 +0300

    Lib DONE

---
+++ Functions/ArraysLibTest/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<FillRand.h>
+#include<Print.h>
+#include<Sort.h>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+}
