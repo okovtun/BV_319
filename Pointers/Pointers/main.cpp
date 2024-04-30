10432bb131f8689337b4e5c13bde1a50c22d694f
Clayman <Clayman@DESKTOP-DM8E77U>
2024-04-30 21:54:17 +0300

    Pointers day 1

---
+++ Pointers/Pointers/main.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define POINTERS_BASICS
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+#ifdef POINTERS_BASICS
+	int a = 2;
+	int* pa = &a;
+	cout << a << endl;
+	cout << &a << endl;	//Взятие адреса переменной 'a' прямо при выводе.
+	cout << pa << endl;	//Вывод адреса, хранящегося в указателе
+	cout << *pa << endl;//Разыменовываем указатель 'pa' и получаем значение, хранящееся по адресу.
+
+	int* pb;
+	int b = 3;
+	pb = &b;
+
+	//int  - 'int'
+	//int* - Указатель на 'int'
+	//double  - 'double'
+	//double* - Указатель на 'double'
+	//char  - 'char'
+	//char* - Указатель на 'char'  
+#endif // POINTERS_BASICS
+
+	const int n = 5;
+	short arr[n] = { 3, 5, 8, 13, 21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+	cout << endl;
+}
+
+/*
+---------------------------------------
+& - Оператор взятия адреса
+	(Address-of operator)
+
+* - Оператор разыменования
+	(Dereference operator)
+---------------------------------------
+*/
+
+/*
+---------------------------------------
++
+-
+++
+--
+---------------------------------------
+*/
