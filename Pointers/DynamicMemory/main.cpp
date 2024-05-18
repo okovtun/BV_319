8ac66eb49660e68f5cd84429466b72c7b90d40dd
Clayman <Clayman@DESKTOP-DM8E77U>
2024-05-18 22:11:09 +0300

    Dynamic memory 2

--- Pointers/DynamicMemory/main.cpp
+++ Pointers/DynamicMemory/main.cpp
@@ -1,10 +1,16 @@
 ﻿#include<iostream>
 using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
 
 #define tab "\t"
 
 void FillRand(int arr[], const int n);
+void FillRand(int** arr, const int rows, const int cols);
+
 void Print(int arr[], const int n);
+void Print(int** arr, const int rows, const int cols);
 
 int* push_back(int arr[], int& n, const int value);
 int* push_front(int arr[], int& n, const int value);
@@ -14,9 +20,14 @@ int* pop_back(int arr[], int& n);
 int* pop_front(int arr[], int& n);
 int* erase(int arr[], int& n, const int index);
 
+//#define DYNAMIC_MEMORY_1
+#define DYNAMIC_MEMORY_2
+
 void main()
 {
 	setlocale(LC_ALL, "");
+
+#ifdef DYNAMIC_MEMORY_1
 	//		new
 	int n = 5;
 	cout << "Введите размер массива: "; cin >> n;
@@ -51,7 +62,66 @@ void main()
 	Print(arr, n);
 
 	delete[] arr;
-	//Memory leaks
+	//Memory leaks  
+#endif // DYNAMIC_MEMORY_1
+
+	int rows;
+	int cols;
+	cout << "Введите количество строк: "; cin >> rows;	//Enter number of rows
+	cout << "Введите количество элементов строки: "; cin >> cols;
+
+	int** arr = new int*[rows];
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	delete[] arr;
+
+#ifdef SYNTAX
+	int rows = 3;
+	int cols = 4;
+
+	///////////////////////////////////////////////////////////////
+	///		   Объявление двумерного динамического массива		///
+	///////////////////////////////////////////////////////////////
+
+	//1) Создаем массив указателей:
+	int** arr = new int*[rows];
+
+	//2) Выделяем память под строки:
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
+
+	///////////////////////////////////////////////////////////////
+	///	Обращение к элементам двумерного динамического массива	///
+	///////////////////////////////////////////////////////////////
+
+	FillRand(arr, rows, cols);
+	Print(arr, rows, cols);
+
+	///////////////////////////////////////////////////////////////
+	///		   Удаление двумерного динамического массива		///
+	///////////////////////////////////////////////////////////////
+
+	//1) Удаляем строки:
+	for (int i = 0; i < rows; i++)
+	{
+		delete[] arr[i];
+	}
+	//2) Удаляем массив указателей:
+	delete[] arr;
+#endif // SYNTAX
+
 }
 
 void FillRand(int arr[], const int n)
@@ -62,6 +132,17 @@ void FillRand(int arr[], const int n)
 		//Обращение к элементам массива через арифметику указателей и оператор разыменования
 	}
 }
+void FillRand(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+
 void Print(int arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
@@ -71,7 +152,18 @@ void Print(int arr[], const int n)
 	}
 	cout << endl;
 }
-
+void Print(int** arr, const int rows, const int cols)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
 int* push_back(int arr[], int& n, const int value)
 {
 	//1) Создаем буферный массив нужного мазмера:
